f5417cad270cdd8f6adbe18076feb6f049eaf914
Benqqa <62963388+olkagolchik@users.noreply.github.com>
2021-11-22 23:19:11 +0300

    test

--- homework1/main.cpp
+++ homework1/main.cpp
@@ -1,36 +1,9 @@
 #include <iostream>
 #include <fstream>
-#include <iomanip>
 #include <valarray>
 
 using namespace std;
-string * StringToMass(string base_str, char delim, int size){
 
-    size_t pos = 0;
-    size_t base_str_size = base_str.size();
-    size_t delim_size=1;
-    string temp;
-    int i=0;
-    string *mass_str = new string[size];
-    while (pos < base_str_size) {
-        temp = temp.assign(base_str, pos, base_str.find(delim, pos) - pos);
-        if (temp.size() > 0)
-        {
-            mass_str[i] = temp;
-            i++;
-            if(i==size){
-                break;
-            }
-        }
-        pos += temp.size() + delim_size;
-
-    }
-
-    return mass_str;
-}
-std::tuple<int, int> step(int dividend, int divisor) {
-    return  std::make_tuple(dividend / divisor, dividend % divisor);
-}
 int main() {
     std::string line;
     double xn=0,yn=0,fx_r =0,fy_r=0,fx_l=0,fy_l=0;
@@ -40,30 +13,12 @@ int main() {
     bool isFirstLine= true;
     bool isTwiceLine_r= true,isTwiceLine_l= true;
     std::ifstream in("in.txt");
-    if (in.is_open())
-    {
+        in >> xn >> yn;
+
         double x =0;
         double y=0;
-        while (getline(in, line))
+        while (in >> x >> y)
         {
-
-            if(line.length() == 0){
-                continue;
-            }
-            string *values=StringToMass(line,' ',2);
-
-            x=atof( values[0].c_str() );
-            y=atof( values[1].c_str() );
-
-            if(isFirstLine){
-                isFirstLine= false;
-                xn=x;
-                yn=y;
-                if(x==0 && y==0){
-                    break;
-                }
-            }
-            else{
                 if(x==0 && y==0){
                     continue;
                 }
@@ -83,11 +38,7 @@ int main() {
                     fy_l = y;
                 }
             }
-            delete[] values;
-        }
-    }
     in.close();
-
     std::cout << "Leftmost: " << fx_r << " " << fy_r << "\n";
     std::cout << "Rightmost: " << fx_l << " " << fy_l << "\n";
 
