80281f846eef2ae194a5867db1ad21ebbaab3467
Benqqa <62963388+olkagolchik@users.noreply.github.com>
2021-11-22 23:23:49 +0300

    Update main.cpp

--- homework1/main.cpp
+++ homework1/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main() {
     std::string line;
-    double xn=0,yn=0,fx_r =0,fy_r=0,fx_l=0,fy_l=0;
+    int xn=0,yn=0,fx_r =0,fy_r=0,fx_l=0,fy_l=0;
     double finde_value=1;
     double min_d_r=1;
     double min_d_l=1;
@@ -15,23 +15,23 @@ int main() {
     std::ifstream in("in.txt");
         in >> xn >> yn;
 
-        double x =0;
-        double y=0;
+        int x =0;
+        int y=0;
         while (in >> x >> y)
         {
                 if(x==0 && y==0){
                     continue;
                 }
-                double _cos=(xn*x+yn*y)/(sqrt(pow(x,2)+pow(y,2))*sqrt(pow(xn,2)+pow(yn,2)));
+                double _cos=(xn*x+yn*y)/(sqrt(x*x+y*y)*sqrt(xn*xn+yn*yn));
                 _cos = std::round(_cos * 10000000000.0) / 10000000000.0;
-                if (_cos-min_d_l <= 0 && ((xn-0)*(y-0)-(yn-0)*(x-0))>0)
+                if (_cos-min_d_l <= 0 && (xn*y-yn*x)>0)
                 {
                     min_d_l = _cos;
                     fx_r = x;
                     fy_r = y;
 
                 }
-                if (_cos-min_d_r <= 0 && ((xn-0)*(y-0)-(yn-0)*(x-0))<=0)
+                if (_cos-min_d_r <= 0 && (xn*y-yn*x)<=0)
                 {
                     min_d_r = _cos;
                     fx_l = x;
